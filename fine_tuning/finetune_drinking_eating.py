40580416da343cc0e19531a63959c0bacc617361
laoli518 <167960412+laoli518@users.noreply.github.com>
2026-06-01 19:20:29 +0800

    Delete fine_tuning directory

--- fine_tuning/finetune_drinking_eating.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from fine_tuning.folder_adapter_finetune import main
-if "--preset" not in sys.argv:
-    sys.argv.extend(["--preset", "drinking_eating"])
-if __name__ == "__main__":
-    main()
--- fine_tuning/finetune_fight_nofight.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from fine_tuning.folder_adapter_finetune import main
-if __name__ == "__main__":
-    main()
