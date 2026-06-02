7cbe840bfb830bd6e211426a6c13b828c6a3395f
laoli518 <167960412+laoli518@users.noreply.github.com>
2026-06-02 19:16:23 +0800

    Delete src/oracle directory

--- src/oracle/constants.py
+++
@@ -1,4 +0,0 @@
-"""Media extension constants used throughout the project."""
-
-VIDEO_EXTENSIONS = {".mp4", ".avi", ".mov", ".mkv", ".wmv", ".flv", ".m4v", ".ts"}
-IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp", ".gif"}
